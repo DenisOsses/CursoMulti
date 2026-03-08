e1c698486151063660999a237a82f17e65166071
Denis Osses <doosses@gmail.com>
2026-03-08 19:04:44 -0300

    Update documentation

---
+++ _static/google_analytics.js
@@ -0,0 +1,13 @@
+// Google Analytics 4 - G-0ZZM6N18QQ
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-0ZZM6N18QQ';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-0ZZM6N18QQ');
+})();
